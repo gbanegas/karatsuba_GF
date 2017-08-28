e696579a5ee4b8c15a7652cfc4a490135529bc39
Gustavo Banegas <gustavo@cryptme.in>
2017-08-28 23:35:45 +0200

    initial commit

---
+++ karatsuba.srcs/sources_1/new/multiplication_wrapper.vhd
@@ -0,0 +1,154 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 08/28/2017 11:10:42 AM
+-- Design Name: 
+-- Module Name: MultiWraper - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity MultiWraper is
+--  Port ( );
+generic (M: INTEGER:= 163);
+PORT ( CLK: IN STD_LOGIC;
+       RSTN : IN STD_LOGIC;
+       SET : IN STD_LOGIC;
+       GET : IN STD_LOGIC;
+       SEL : IN STD_LOGIC_VECTOR(31 DOWNTO 0);  
+       IN0 : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+       IN1 : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+       OUT0: OUT STD_LOGIC_VECTOR(31 DOWNTO 0)
+      );
+
+end MultiWraper;
+
+ARCHITECTURE Behavioral OF MultiWraper IS
+
+COMPONENT polynom_multiplier
+generic (M: integer:= 163);
+port (
+    CLK: IN STD_LOGIC;
+  a, b: in std_logic_vector(M-1 downto 0);
+  
+  d: out std_logic_vector(2*M-2 downto 0)
+);
+end component;
+
+COMPONENT reduction
+generic (M: INTEGER:= 163;
+         BL: INTEGER:= 63;
+         CL: INTEGER:= 37);
+port(
+   CLK: IN STD_LOGIC;
+   RSTN : IN STD_LOGIC; 
+   IN0 : IN STD_LOGIC_VECTOR(2*M-2 DOWNTO 0);
+   OUTPUT: OUT STD_LOGIC_VECTOR(M-1 DOWNTO 0)
+);
+end component;
+
+
+SIGNAL A, B	: STD_LOGIC_VECTOR (M-1 DOWNTO 0);
+SIGNAL RESULT: STD_LOGIC_VECTOR (2*M-2 DOWNTO 0);
+SIGNAL RESULT_REG: STD_LOGIC_VECTOR (M-1 DOWNTO 0);
+
+BEGIN
+
+    MULT: polynom_multiplier 
+    GENERIC MAP ( M => M)
+    PORT MAP(
+        CLK => CLK,
+        a => A,
+        b => B,
+        d => RESULT
+    );
+    
+    RED: reduction GENERIC MAP ( M => M, BL => 63, CL => 37)
+         PORT MAP(
+            CLK => CLK,
+            RSTN => RSTN,   
+            IN0 => RESULT,
+            OUTPUT => RESULT_REG
+    );     
+    
+    IREGISTERS : PROCESS(CLK)
+        BEGIN
+            IF RISING_EDGE(CLK) THEN
+                 if(SET = '1') THEN
+                case SEL is
+                  when "001" =>   
+                   A(31 DOWNTO 0)            <= IN0; --& A(6 DOWNTO 32);Z <= A;
+                   B(31 DOWNTO 0)            <= IN1; --& B(255 DOWNTO 32);
+                  when "010" =>   
+                  A(63 DOWNTO 32)            <= IN0; --& A(6 DOWNTO 32);Z <= A;
+                  B(63 DOWNTO 32)            <= IN1; --& B(255 DOWNTO 32);
+                  when "011" =>   
+                  A(95 DOWNTO 64)            <= IN0; --& A(6 DOWNTO 32);Z <= A;
+                  B(95 DOWNTO 64)            <= IN1; --& B(255 DOWNTO 32);
+                  when "100" =>   
+                  A(127 DOWNTO 96)            <= IN0; --& A(6 DOWNTO 32);Z <= A;
+                  B(127 DOWNTO 96)            <= IN1; --& B(255 DOWNTO 32);
+                  when "101" =>   
+                  A(155 DOWNTO 128)            <= IN0(27 DOWNTO 0); --& A(6 DOWNTO 32);Z <= A;
+                  B(155 DOWNTO 128)            <= IN1(27 DOWNTO 0); --& B(255 DOWNTO 32);
+                  when others => 
+                  A            <= A;
+                  B            <= B;
+                end case;
+                end if;
+            END IF;
+        END PROCESS;
+        
+        
+           
+        
+        OREGISTER : PROCESS(CLK)
+         BEGIN
+                   IF RISING_EDGE(CLK) THEN
+                        if(GET = '1') THEN
+                       case SEL is
+                         when "001" =>   
+                          OUT0            <= RESULT_REG(31 DOWNTO 0); --& A(6 DOWNTO 32);Z <= A;
+                         when "010" =>   
+                         OUT0          <= RESULT_REG(63 DOWNTO 32); --& A(6 DOWNTO 32);Z <= A;
+                         when "011" =>   
+                         OUT0           <= RESULT_REG(95 DOWNTO 64); --& A(6 DOWNTO 32);Z <= A;
+                         when "100" =>   
+                         OUT0            <= RESULT_REG(127 DOWNTO 96)   ; --& A(6 DOWNTO 32);Z <= A;
+                         when "101" =>   
+                         OUT0            <= "0000" & RESULT_REG(155 DOWNTO 128); --& A(6 DOWNTO 32);Z <= A;
+                         when others => 
+                         OUT0            <= (OTHERS => '0');
+                       end case;
+                       end if;
+                   END IF;
+               END PROCESS;
+    
+     
+
+END Behavioral;
---
+++ karatsuba.srcs/sources_1/new/reduction.vhd
@@ -0,0 +1,128 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 08/28/2017 01:15:40 PM
+-- Design Name: 
+-- Module Name: reduction - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.all;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use ieee.numeric_std.all;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity reduction is
+generic (
+         M: integer:= 163;
+         BL: INTEGER:= 63;
+         CL:INTEGER:= 37
+         );
+  Port ( 
+   CLK: IN STD_LOGIC;
+   RSTN : IN STD_LOGIC;
+   IN0 : IN STD_LOGIC_VECTOR(2*M-2 DOWNTO 0);
+   OUTPUT: OUT STD_LOGIC_VECTOR(M-1 DOWNTO 0)
+  );
+end reduction;
+
+architecture structural of reduction is
+ signal d_result: STD_LOGIC_VECTOR(M-1 downto 0);
+ signal T_1: STD_LOGIC_VECTOR(M-1 downto 0);
+begin
+
+process(CLK)
+begin
+ if clk'event and clk = '1' then
+  for i in 0 to BL-2 loop
+              T_1(i) <= IN0(i+2*BL+1) xor IN0(i+3*BL+2*CL);
+  end loop;
+ --GENERATE_T1: for i in 0 to BL-2 generate
+ --             T_1(i) <= IN0(i+2*BL+1) xor IN0(i+3*BL+2*CL);
+ --            end generate;
+ for i in 0 to CL-1 loop
+              d_result(i) <= IN0(i) xor T_1(i);
+ end loop;
+ --REDUCTION_1: for i in 0 to CL-1 generate
+ --             d_result(i) <= IN0(i) xor T_1(i);
+  --           end generate;
+ for i in CL to BL-2 loop
+              d_result(i) <= IN0(i) xor T_1(i) xor IN0(i+2*BL);
+ end loop;
+ --REDUCTION_2: for i in CL to BL-2 generate
+ --            d_result(i) <= IN0(i) xor T_1(i) xor IN0(i+2*BL);
+ --            end generate;
+ 
+ d_result(BL-1) <= IN0(BL-1) xor IN0(3*BL+CL-1) xor IN0(3*BL -1);
+ for i in BL to BL+CL-2 loop
+               d_result(i) <= IN0(i) xor T_1(i-BL) xor IN0(i+2*BL);
+ end loop;
+-- REDUCTION_3: for i in BL to BL+CL-2 generate
+ --              d_result(i) <= IN0(i) xor T_1(i-BL) xor IN0(i+2*BL);
+ --             end generate;
+ 
+-- d_result(BL+CL-1) <= IN0(BL+CL-1) xor IN0(3*BL+CL-1) xor T_1(CL-1);
+ for i in BL+CL to 2*BL-2 loop
+              d_result(i) <= IN0(i) xor T_1(i-BL) xor T_1(i-BL-CL); 
+ end loop;
+ 
+ --REDUCTION_4: for i in BL+CL to 2*BL-2 generate
+ --             d_result(i) <= IN0(i) xor T_1(i-BL) xor T_1(i-BL-CL); 
+ --             end generate;
+ 
+d_result(2*BL-1) <= IN0(2*BL-1) xor IN0(3*BL+CL-1) xor T_1(BL-CL-1);
+
+for i in 2*BL to 2*BL+CL-2 loop
+               d_result(i) <= IN0(i) xor T_1(i-BL-CL)  xor IN0(i+BL+CL);
+ end loop;
+ 
+ --REDUCTION_5: for i in 2*BL to 2*BL+CL-2 generate
+ --              d_result(i) <= IN0(i) xor T_1(i-BL-CL)  xor IN0(i+BL+CL);
+ --             end generate;
+ 
+ d_result(2*BL+CL-1) <= IN0(2*BL+CL-1) xor IN0(3*BL+CL-1) xor IN0(3*BL-1);
+ 
+ for i in 0 to CL-1 loop
+                d_result(i) <= d_result(i) xor IN0(i+3*BL+CL);
+ end loop;
+ 
+ --REDUCTION_6: for i in 0 to CL-1 generate
+ --              d_result(i) <= d_result(i) xor IN0(i+3*BL+CL);
+ --             end generate;
+ for i in CL to BL+2*CL-2 loop
+               d_result(i) <= d_result(i) xor IN0(i+3*BL);
+ end loop;
+ --REDUCTION_7: for i in CL to BL+2*CL-2 generate
+ --              d_result(i) <= d_result(i) xor IN0(i+3*BL);
+ --             end generate;
+
+    OUTPUT <= d_result;
+ end if;
+end process;
+
+end structural;
+
+
+
